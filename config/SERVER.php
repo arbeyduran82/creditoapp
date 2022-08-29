5683516edc12cfb9d9a278f3778cd4242414969b
Diosmar Arbey Cardenas Duran <developer@Developer.local>
2022-08-29 16:48:32 -0500

    Se definen parametros globales, de conexion a la base de datos y de encritacion

---
+++ config/SERVER.php
@@ -0,0 +1,13 @@
+<?php
+const SERVER = "localhost";
+const DB = "micredito";
+const USER = "root";
+const PASS = "";
+
+//definicion de constante para conexion PDO
+const SGBD ="mysql:host=".SERVER.";dbname=".DB;
+
+//definicion constantes con parametros de encriptacion
+const METHOD ="AES-256-CBC";
+const KEY = '%CREDITOS@2022';
+const KEY2 = '240982';
