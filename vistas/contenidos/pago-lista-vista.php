8d77e52cc0194a8f0030b8397c0c633708af8ded
angie r <alreina240@misena.edu.co>
2022-10-16 22:58:41 -0500

    botones eliminar

--- vistas/contenidos/pago-lista-vista.php
+++ vistas/contenidos/pago-lista-vista.php
@@ -60,7 +60,7 @@
 							</td>
 							<td>
 								<button type="button" class="btn btn-warning">
-									<a href="../controladores/eliminarPagControlador.php?id=<?php echo $key['pag_id'] ?>" class="btn btn-danger"><i class="far fa-trash-alt"></i></a>
+									<a href="<?php echo SERVERURL; ?>controladores/eliminarPagControlador.php?id=<?php echo $key['pag_id'] ?>" class="btn btn-danger"><i class="far fa-trash-alt"></i></a>
 								</button>	
 							</td>
 							</tr>
